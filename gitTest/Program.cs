1f66e4243bd3c045022a82067748988461be8b85
user <user@DESKTOP-05J4HLG>
2016-06-30 02:14:17 +0200

    видит окно закрытия.

--- gitTest/Program.cs
+++ gitTest/Program.cs
@@ -971,8 +971,78 @@ namespace gitTest
         /// </summary>
         HSHELL_WINDOWREPLACED = 13
     }
+    /// <summary>
+    /// Retrieves a handle to a window that has the specified relationship (Z-Order or owner) to the specified window.
+    /// </summary>
+    /// <remarks>The EnumChildWindows function is more reliable than calling GetWindow in a loop. An application that
+    /// calls GetWindow to perform this task risks being caught in an infinite loop or referencing a handle to a window
+    /// that has been destroyed.</remarks>
+    /// <param name="hWnd">A handle to a window. The window handle retrieved is relative to this window, based on the
+    /// value of the uCmd parameter.</param>
+    /// <param name="uCmd">The relationship between the specified window and the window whose handle is to be
+    /// retrieved.</param>
+    /// <returns>
+    /// If the function succeeds, the return value is a window handle. If no window exists with the specified relationship
+    /// to the specified window, the return value is NULL. To get extended error information, call GetLastError.
+    /// </returns>
+   
+
+    enum GetWindowType : uint
+    {
+        /// <summary>
+        /// The retrieved handle identifies the window of the same type that is highest in the Z order.
+        /// <para/>
+        /// If the specified window is a topmost window, the handle identifies a topmost window.
+        /// If the specified window is a top-level window, the handle identifies a top-level window.
+        /// If the specified window is a child window, the handle identifies a sibling window.
+        /// </summary>
+        GW_HWNDFIRST = 0,
+        /// <summary>
+        /// The retrieved handle identifies the window of the same type that is lowest in the Z order.
+        /// <para />
+        /// If the specified window is a topmost window, the handle identifies a topmost window.
+        /// If the specified window is a top-level window, the handle identifies a top-level window.
+        /// If the specified window is a child window, the handle identifies a sibling window.
+        /// </summary>
+        GW_HWNDLAST = 1,
+        /// <summary>
+        /// The retrieved handle identifies the window below the specified window in the Z order.
+        /// <para />
+        /// If the specified window is a topmost window, the handle identifies a topmost window.
+        /// If the specified window is a top-level window, the handle identifies a top-level window.
+        /// If the specified window is a child window, the handle identifies a sibling window.
+        /// </summary>
+        GW_HWNDNEXT = 2,
+        /// <summary>
+        /// The retrieved handle identifies the window above the specified window in the Z order.
+        /// <para />
+        /// If the specified window is a topmost window, the handle identifies a topmost window.
+        /// If the specified window is a top-level window, the handle identifies a top-level window.
+        /// If the specified window is a child window, the handle identifies a sibling window.
+        /// </summary>
+        GW_HWNDPREV = 3,
+        /// <summary>
+        /// The retrieved handle identifies the specified window's owner window, if any.
+        /// </summary>
+        GW_OWNER = 4,
+        /// <summary>
+        /// The retrieved handle identifies the child window at the top of the Z order,
+        /// if the specified window is a parent window; otherwise, the retrieved handle is NULL.
+        /// The function examines only child windows of the specified window. It does not examine descendant windows.
+        /// </summary>
+        GW_CHILD = 5,
+        /// <summary>
+        /// The retrieved handle identifies the enabled popup window owned by the specified window (the
+        /// search uses the first such window found using GW_HWNDNEXT); otherwise, if there are no enabled
+        /// popup windows, the retrieved handle is that of the specified window.
+        /// </summary>
+        GW_ENABLEDPOPUP = 6
+    }
     class Program
     {
+        [DllImport("user32.dll", SetLastError = true)]
+        private static extern IntPtr GetWindow(IntPtr hWnd, GetWindowType uCmd);
+
         [DllImport("user32.dll", SetLastError = true)]
         static extern IntPtr FindWindow(string lpClassName, string lpWindowName);
 
@@ -983,18 +1053,29 @@ namespace gitTest
         [DllImport("user32.dll", SetLastError = true, CharSet = CharSet.Auto)]
         static extern bool PostMessage(HandleRef hWnd, uint Msg, IntPtr wParam, IntPtr lParam);
 
+        [DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        static extern int GetWindowText(IntPtr hWnd, StringBuilder lpString, int nMaxCount);
+
         static void Main(string[] args)
         {
             IntPtr ptr = FindWindow("CAO.Faktura", null);
-            
             if (ptr==null)
             {
-                Console.WriteLine("null");
+                Console.WriteLine("CAO nicht started!");
             }
             else
             {
+                StringBuilder captions = new StringBuilder();
                 Console.WriteLine(ptr.ToString());
                 PostMessage(new HandleRef(null, ptr), (uint)WM.CLOSE, (System.IntPtr)0, (System.IntPtr)0);
+                ptr = GetWindow(ptr, GetWindowType.GW_CHILD);
+                while (ptr != null)
+                {
+                    GetWindowText(ptr, captions, 500);
+                    Console.WriteLine(captions.ToString());
+                    captions.Clear();
+                    ptr = GetWindow(ptr, GetWindowType.GW_HWNDNEXT);
+                }
             }
         }
     }
